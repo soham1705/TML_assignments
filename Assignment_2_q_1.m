b8e5bf7d934d861e3bbe41aff079d4f7f60962b8
Soham Marik <soham.m15@iiits.in>
2018-03-17 11:14:21 +0530

    Create nodes matrix

--- Assignment_2_q_1.m
+++ Assignment_2_q_1.m
@@ -1,1 +1,14 @@
-w=randn(iters,1);
+nodes=randi(100,1,1);
+iters=input("Enter no. of interations:");
+w=randn(nodes,iters);
+neighbours=randi([0,1],nodes,nodes);
+for i=1:nodes
+    for j=1:i
+        neighbours(i,j)=neighbours(j,i);
+        if(j==i)
+            neighbours(i,j)=0;
+        end
+            
+    end
+end
+
