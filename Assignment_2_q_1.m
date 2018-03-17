ee3baa30165d7a643067edcf142201c190f45f45
Soham Marik <soham.m15@iiits.in>
2018-03-17 11:44:07 +0530

    Create 'a' matrix

--- Assignment_2_q_1.m
+++ Assignment_2_q_1.m
@@ -1,14 +1,29 @@
-nodes=randi(100,1,1);
-iters=input("Enter no. of interations:");
-w=randn(nodes,iters);
+nodes=randi(10,1,1);
+%iters=input("Enter no. of interations:");
+%w=randn(nodes,iters);
 neighbours=randi([0,1],nodes,nodes);
+
 for i=1:nodes
     for j=1:i
         neighbours(i,j)=neighbours(j,i);
         if(j==i)
             neighbours(i,j)=0;
         end
-            
     end
 end
+
+a=neighbours;
+for i=1:nodes
+    sum=0;
+    for j=1:nodes
+        sum=sum+neighbours(i,j);
+    end
+    for j=1:nodes
+        a(i,j)=neighbours(i,j)/sum;
+    end
+end
+
+
+
+
 
