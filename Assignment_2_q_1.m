903f240d076fe5ef705fe7765740d849393ef5b2
Soham Marik <soham.m15@iiits.in>
2018-03-26 21:40:39 +0530

    ATC formula

--- Assignment_2_q_1.m
+++ Assignment_2_q_1.m
@@ -2,6 +2,7 @@ nodes=randi(10,1,1);
 iters=input('Enter no. of iterations:');
 w=randn(nodes,iters);
 u=randn(nodes,iters);
+mu=0.01;
 neighbours=randi([0,1],nodes,nodes);
 
 for i=1:nodes
@@ -25,8 +26,38 @@ for i=1:nodes
 end
 
 c=a;
-
 d=randi([0,1],nodes,1);
+
+psi=zeros(nodes,iters);
+
+for k=1:nodes
+    for i=1:iters
+        neighbour_sum=0;
+        for l=1:nodes
+            if(neighbours(k,l)==1)
+                if(i>1)
+                    neighbour_sum=neighbour_sum+c(l,k)*u(l,i)*(d(l)-u(l,i)*w(k,i-1));
+                elseif(i==1)
+                     neighbour_sum=neighbour_sum;
+                end
+            end
+        end
+        if(i>1)
+            psi(k,i)=w(k,i-1)+mu*neighbour_sum;
+        elseif(i==1)
+            psi=mu*neighbour_sum;
+        end
+        a_psi_sum=0;
+        for l=1:nodes
+            if(neighbours(k,l)==1)
+                a_psi_sum=a_psi_sum+a(l,k)*psi(l,i);
+            end
+        end
+        w(k,i)=a_psi_sum;
+    end
+end
+
+    
 
 
 
