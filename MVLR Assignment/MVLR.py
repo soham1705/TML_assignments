6ad1511b165635d0c5b65840ff9c95307eee9199
Soham Marik <soham.m15@iiits.in>
2018-03-30 12:15:39 +0530

    Find train error

--- MVLR Assignment/MVLR.py
+++ MVLR Assignment/MVLR.py
@@ -14,10 +14,14 @@ df = pd.read_csv('Bridges_Data.txt',\
 tf = pd.read_csv('Bridges_Test.txt',\
                  names=['Id','SRR', 'FRR', 'SUR', 'ERR', 'RiskScore'])
 
+#training set
+
 X = df[['SRR','FRR','SUR','ERR']]
 Y = df['RiskScore']
 
 reg = LinearRegression()
+
+#test set
 
 X_ = tf[['SRR','FRR','SUR','ERR']]
 Y_ = tf['RiskScore']
@@ -26,13 +30,22 @@ for i in range(1,7):
     print "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
     print "%%%% Multivariate Linear Regression Of Order ",i," %%%%%%%%%"
     print "\n"
+
     poly = PolynomialFeatures(degree = i)
     X1 = poly.fit_transform(X)
     reg.fit(X1,Y)
     print "Score: ",reg.score(X1,Y)
+    Y_p_t=reg.predict(X1)
+
     X1 = poly.fit_transform(X_)
     Y_p = reg.predict(X1)
-    print "Coefficients: ",reg.coef_
-    m_sq_err = mean_squared_error(Y_, Y_p)
-    print "Mean Squared Error: ", m_sq_err
+    print Y_p.shape
+    print Y.shape
+    print Y_.shape
+
+    #print "Coefficients: ",reg.coef_
+    train_err = mean_squared_error(Y, Y_p_t)
+    test_err = mean_squared_error(Y_, Y_p)
+    print "Train Error: ",  train_err
+    print "Test Error: ", test_err
     print "\n\n\n\n\n"
